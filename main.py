66f77e4b575a0dfce99d048b96133c0d5ac070de
ShadowFokx <83240429+ShadowFokx@users.noreply.github.com>
2022-06-02 22:42:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import board
-from kmk.kmk_keyboard import KMKKeyboard
-from kmk.keys import KC
-from kmk.scanners import DiodeOrientation
 from kmk.extensions.RGB import RGB
-from kb import rgb_pixel_pin
-from kmk.extensions.RGB import RGB, AnimationModes
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners import DiodeOrientation
+from kmk.keys import KC
 
-print("Starting")
+print("start")
+
+
 keyboard = KMKKeyboard()
+
+keyboard.debug_enabled = True
 
 keyboard.col_pins = (board.GP7, board.GP9)  # try D5 on Feather, keeboar
 keyboard.row_pins = (board.GP8,)  # try D6 on Feather, keeboar
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
-print("begin key")
 keyboard.keymap = [[KC.A, KC.B]]
 
-print("bgin rgb")
-rgb_ext = RGB(
-    pixel_pin=board.GP15,
-    num_pixels=2,
-    animation_mode=AnimationModes.RAINBOW,
-)
-print("end")
-if __name__ == "__main__":
+
+rgb_ext = RGB(pixel_pin=board.GP15, num_pixels=2)
+keyboard.extensions.append(rgb_ext)
+
+
+if __name__ == '__main__':
     keyboard.go()
+
+    
